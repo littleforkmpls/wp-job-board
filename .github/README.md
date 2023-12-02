64e5a37351148352f12df7c47ecbc5ade3bf2870
Drew Brown <dbrown78@gmail.com>
2023-12-01 20:04:40 -0600

    Add archive builder and updates to re make 0.1.0

--- .github/README.md
+++ .github/README.md
@@ -1,1 +1,5 @@
 # WP Job Board
+
+### Releases
+To create a release run `./bin/build-zip.sh` from a shell.  Then use the archive created in dist
+(`dist/wp-job-board.{VER}.zip`) to attache to the release in github.
---
+++ bin/build-zip.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+VER=${1-0.1.0}
+DIST_DIR=${2-./dist}
+DIR_TO_ZIP=wp-job-board
+ARCHIVE=wp-job-board.$VER.zip
+
+if [ -f $DIST_DIR/$ARCHIVE ]; then
+    rm $DIST_DIR/$ARCHIVE;
+fi
+
+mkdir -p $DIST_DIR/$DIR_TO_ZIP
+
+cp -af admin $DIST_DIR/$DIR_TO_ZIP
+cp -af includes $DIST_DIR/$DIR_TO_ZIP
+cp -af languages $DIST_DIR/$DIR_TO_ZIP
+cp -af public $DIST_DIR/$DIR_TO_ZIP
+cp wp-job-board.php $DIST_DIR/$DIR_TO_ZIP
+cp uninstall.php $DIST_DIR/$DIR_TO_ZIP
+cp index.php $DIST_DIR/$DIR_TO_ZIP
+cp readme.txt $DIST_DIR/$DIR_TO_ZIP
+
+cd $DIST_DIR && zip -r $ARCHIVE  $DIR_TO_ZIP
+
+rm -rf $DIR_TO_ZIP
