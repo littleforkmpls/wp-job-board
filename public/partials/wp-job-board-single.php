14a664bd08cd8cf1a1492f61207cf2966a3816a8
Justin Lawson <jstn.lawson@gmail.com>
2023-12-13 15:28:30 -0600

    commit wpjobboard

--- public/partials/wp-job-board-single.php
+++ public/partials/wp-job-board-single.php
@@ -1,5 +1,19 @@
 <h1>WPJB Plugin Single</h1>
 <h2><?php the_title(); ?></h2>
+
+<?php
+    $post_meta_single = get_post_meta(get_the_ID(), 'wp_job_board_bh_data', true);
+    $post_meta_single_decode = json_decode($post_meta_single);
+
+    $post_publish_date = $post_meta_single_decode->dateLastPublished;
+    $post_employment_type = $post_meta_single_decode->employmentType;
+?>
+
+<div>Employment Type: <?php echo $post_employment_type; ?></div>
+<div>Publish Date: <?php echo $post_publish_date; ?></div>
+
+
+
 <div class="wpjb-fullCard__btn-container">
       <button class="wpjb-fullCard__back-btn"><a href="./index.html">˂ Back</a></button>
     </div>
