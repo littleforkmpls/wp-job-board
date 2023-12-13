ae9952c10b54ffbf128f800fb5ac27f726d07e32
Justin Lawson <jstn.lawson@gmail.com>
2023-12-13 12:40:33 -0600

    html and css for fullCard added

--- public/partials/wp-job-board-single.php
+++ public/partials/wp-job-board-single.php
@@ -1,2 +1,46 @@
 <h1>WPJB Plugin Single</h1>
 <h2><?php the_title(); ?></h2>
+<div class="wpjb-fullCard__btn-container">
+      <button class="wpjb-fullCard__back-btn"><a href="./index.html">˂ Back</a></button>
+    </div>
+    <div class="wpjb-fullCard">
+      <div class="wpjb-fullCard__hd">
+        <h2 class="txt txt--h3">Boulder Mover</h2>
+        <div>
+          <button class="wpjb-fullCard__utility-btn">📩</button>
+          <button class="wpjb-fullCard__utility-btn">⎙</button>
+          <button class="wpjb-fullCard__utility-btn">🔖</button>
+        </div>
+      </div>
+      <div class="wpjb-fullCard__job-info">
+        <p>⚖ Posted Nov 11th 2023</p>
+        <p>⏲ Updated 42 minutes ago</p>
+        <p>📍 Milwaukee, WI</p>
+        <p>☑️ Fulltime</p>
+      </div>
+      <div class="wpjb-fullCard__btn-container">
+        <button class="wpjb-fullCard__apply-btn">Apply</button>
+      </div>
+      <div class="wpjb-fullCard__bd">
+        <p class="txt">
+          Pick stuff up and put it in another location. After some time has
+          passed move it to another location. Repeat. Pick stuff up and put it
+          in another location. After some time has passed move it to another
+          location. Repeat. Pick stuff up and put it in another location. After
+          some time has passed move it to another location. Repeat.Pick stuff up
+          and put it in another location. After some time has passed move it to
+          another location. Repeat. Pick stuff up and put it in another
+          location. After some time has passed move it to another location.
+          Repeat. Pick stuff up and put it in another location. After some time
+          has passed move it to another location. Repeat. Pick stuff up and put
+          it in another location. After some time has passed move it to another
+          location. Repeat. Pick stuff up and put it in another location. After
+          some time has passed move it to another location. Repeat. Pick stuff
+          up and put it in another location. After some time has passed move it
+          to another location. Repeat.
+        </p>
+      </div>
+      <div class="wpjb-fullCard__btn-container">
+        <button class="wpjb-fullCard__apply-btn">Apply</button>
+      </div>
+    </div>
