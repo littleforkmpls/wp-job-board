02ccacd900984efeb11d05ee4d69f0614bad9879
Justin Lawson <jstn.lawson@gmail.com>
2023-12-18 16:11:21 -0600

    made a all jobs instead back button with floating arrow

--- public/partials/wp-job-board-single.php
+++ public/partials/wp-job-board-single.php
@@ -78,9 +78,14 @@ $decoded_job_description = html_entity_decode($post_job_description);
 ?>
 
 
-<!-- <div class="wpjb-card__btn-container">
-      <button class="wpjb-card__back-btn"><a href="./index.html">˂ Back</a></button>
-    </div> -->
+<div class="wpjb-btn__container">
+  <button class="wpjb-btn__back">
+    <svg xmlns="http://www.w3.org/2000/svg" height="14" width="12" viewBox="0 0 448 512">
+      <path opacity="1" fill="currentcolor" d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+    </svg>
+    <span>All Jobs</span>
+  </button>
+</div>
 <div class="wpjb-card" id="wpjb-card">
   <div class="wpjb-card__hd">
     <h1 style="font-size: 2rem; font-weight: 500;"><?php the_title(); ?></h1>
