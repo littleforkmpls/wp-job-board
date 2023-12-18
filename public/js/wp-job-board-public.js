4d33ccc06744e9a69d40adbed5c7789072b94883
Justin Lawson <jstn.lawson@gmail.com>
2023-12-17 18:59:24 -0600

    added sub header mores text styles and print function

--- public/js/wp-job-board-public.js
+++ public/js/wp-job-board-public.js
@@ -1,6 +1,8 @@
 (function ($) {
     "use strict";
 
+    // dropdown menu function
+
     $(".wpjb-dropdown").click(function () {
         $(this).toggleClass("is-active");
     });
@@ -8,4 +10,22 @@
     $(".wpjb-dropdown ul").click(function (e) {
         e.stopPropagation();
     });
+
+    // print function
+
+    function printContent() {
+        console.log('print btn clicked!');
+        const content = $('#wpjb-card').html();
+        const printWindow = window.open('', '_blank');
+        printWindow.document.open();
+        printWindow.document.write('<html><head><title>Print</title></head><body>' + content + '</body></html>');
+        printWindow.document.close();
+        printWindow.print();
+        printWindow.onafterprint = function () {
+            printWindow.close();
+        };
+    }
+
+    window.printContent = printContent;
+
 })(jQuery);
--- public/partials/wp-job-board-single.php
+++ public/partials/wp-job-board-single.php
@@ -2,59 +2,59 @@
 <h2><?php the_title(); ?></h2> -->
 
 <?php
-    $post_meta_single = get_post_meta(get_the_ID(), 'wp_job_board_bh_data', true);
-    $post_meta_single_decode = json_decode($post_meta_single);
-    $post_publish_date = $post_meta_single_decode->dateLastPublished;
-    $post_modified_date = $post_meta_single_decode->dateLastModified;
-    $post_employment_type = $post_meta_single_decode->employmentType;
-    $post_city_name = $post_meta_single_decode->address->city;
-    $post_state_name = $post_meta_single_decode->address->state;
-    $post_job_description = $post_meta_single_decode->publicDescription;
+$post_meta_single = get_post_meta(get_the_ID(), 'wp_job_board_bh_data', true);
+$post_meta_single_decode = json_decode($post_meta_single);
+$post_publish_date = $post_meta_single_decode->dateLastPublished;
+$post_modified_date = $post_meta_single_decode->dateLastModified;
+$post_employment_type = $post_meta_single_decode->employmentType;
+$post_city_name = $post_meta_single_decode->address->city;
+$post_state_name = $post_meta_single_decode->address->state;
+$post_job_description = $post_meta_single_decode->publicDescription;
 
-    // Convert publish date to milliseconds
-    $post_modified_date = floor($post_modified_date / 1000);
+// Convert publish date to milliseconds
+$post_modified_date = floor($post_modified_date / 1000);
 
-    //DateTime objects for the post modified date and current time
-    $modifiedDateTime = new DateTime("@$post_modified_date");
-    $currentDateTime = new DateTime();
+//DateTime objects for the post modified date and current time
+$modifiedDateTime = new DateTime("@$post_modified_date");
+$currentDateTime = new DateTime();
 
-    // Calculate the difference with the DateTime::diff method :: is for static methods
-    $interval = $modifiedDateTime->diff($currentDateTime);
+// Calculate the difference with the DateTime::diff method :: is for static methods
+$interval = $modifiedDateTime->diff($currentDateTime);
 
-    // Access the components of the DateInterval
-    $daysDifference = $interval->days;
-    $hoursDifference = $interval->h;
-    $minutesDifference = $interval->i;
-    $secondsDifference = $interval->s;
+// Access the components of the DateInterval
+$daysDifference = $interval->days;
+$hoursDifference = $interval->h;
+$minutesDifference = $interval->i;
+$secondsDifference = $interval->s;
 
-    //variable for formatted time difference
-    $formattedDifference = '';
+//variable for formatted time difference
+$formattedDifference = '';
 
-    // Check the time difference and format accordingly
-  if ($daysDifference > 0) {
-    $formattedDifference = ($daysDifference == 1) ? '1 day' : "$daysDifference days";
-  } elseif ($hoursDifference > 0) {
-    $formattedDifference = ($hoursDifference == 1) ? '1 hour' : "$hoursDifference hours";
-  } elseif ($minutesDifference > 0) {
-    $formattedDifference = ($minutesDifference == 1) ? '1 minute' : "$minutesDifference minutes";
-  } else {
-    $formattedDifference = ($secondsDifference == 1) ? '1 second' : "$secondsDifference seconds";
-  }
-  // Decode HTML entities in job description (not working) 
-  //$decoded_job_description = html_entity_decode($post_job_description);
+// Check the time difference and format accordingly
+if ($daysDifference > 0) {
+  $formattedDifference = ($daysDifference == 1) ? '1 day' : "$daysDifference days";
+} elseif ($hoursDifference > 0) {
+  $formattedDifference = ($hoursDifference == 1) ? '1 hour' : "$hoursDifference hours";
+} elseif ($minutesDifference > 0) {
+  $formattedDifference = ($minutesDifference == 1) ? '1 minute' : "$minutesDifference minutes";
+} else {
+  $formattedDifference = ($secondsDifference == 1) ? '1 second' : "$secondsDifference seconds";
+}
+// Decode HTML entities in job description (not working) 
+//$decoded_job_description = html_entity_decode($post_job_description);
 
-  // $post_job_description = htmlentities($post_job_description, null, 'utf-8'); 
-  // $post_job_description = str_replace(" ", "", $post_job_description);
-  // $post_job_description = preg_replace('/[\s]+/mu', '', $post_job_description);
-  // $post_job_description = str_replace('\u00a0','',$post_job_description);
-  //  echo $post_job_description;
+// $post_job_description = htmlentities($post_job_description, null, 'utf-8'); 
+// $post_job_description = str_replace(" ", "", $post_job_description);
+// $post_job_description = preg_replace('/[\s]+/mu', '', $post_job_description);
+// $post_job_description = str_replace('\u00a0','',$post_job_description);
+//  echo $post_job_description;
 
-  // Replace UTF-8 encoded non-breaking space with a regular space
-  //$cleaned_job_description = preg_replace('/\xc2\xa0/', ' ', $post_job_description);
+// Replace UTF-8 encoded non-breaking space with a regular space
+//$cleaned_job_description = preg_replace('/\xc2\xa0/', ' ', $post_job_description);
 
 
 
-  
+
 
 $cleaned_job_description = str_replace('u00a0', ' ', $post_job_description);
 
@@ -84,7 +84,7 @@ $cleaned_job_description = str_replace('u00a0', ' ', $post_job_description);
 // Remove the unwanted elements
 //$cleaned_job_description = preg_replace('/\bntnttnttntn\b/', '', $cleaned_job_description);
 
- 
+
 // Decode HTML entities
 $decoded_job_description = html_entity_decode($cleaned_job_description);
 ?>
@@ -93,24 +93,27 @@ $decoded_job_description = html_entity_decode($cleaned_job_description);
 <!-- <div class="wpjb-card__btn-container">
       <button class="wpjb-card__back-btn"><a href="./index.html">˂ Back</a></button>
     </div> -->
-    <div class="wpjb-card">
-      <div class="wpjb-card__hd">
-        <h2 class="txt txt--h3"><?php the_title(); ?></h2>
-      </div>
-      <div class="wpjb-card__meta">
-        <p class="txt txt--xs">⚖ Posted <?php echo date('m.d.y', (int) $post_publish_date); ?></p>
-        <p class="txt txt--xs">⏲ Updated <?php echo $formattedDifference;?> ago</p>
-        <p class="txt txt--xs">📍 <?php echo $post_city_name; ?>, <?php echo $post_state_name; ?></p>
-        <p class="txt txt--xs">☑️ <?php echo $post_employment_type; ?></p>
-      </div>
-      <div class="wpjb-card__bd">
-        <p class="txt txt--balance"><?php echo $decoded_job_description; ?></p>
-      </div>
-      <div class="wpjb-card__ft">
-        <div class="wpjb-utilityNav">
-          <button class="wpjb-utilityNav__btn">📩</button>
-          <button class="wpjb-utilityNav__btn">⎙</button>
-          <button class="wpjb-utilityNav__btn">🔖</button>
-        </div>
-      </div>
+<div class="wpjb-card" id="wpjb-card">
+  <div class="wpjb-card__hd">
+    <h2 class="txt txt--h3"><?php the_title(); ?></h2>
+  </div>
+  <div class="wpjb-card__meta">
+    <p class="txt txt--xs"><span class="txt--xl">⚖</span> Posted <?php echo date('m.d.y', (int) $post_publish_date); ?></p>
+    <p class="txt txt--xs txt--500"><span class="txt--xl">⏲</span> Updated <?php echo $formattedDifference; ?> ago</p>
+    <p class="txt txt--xs"><span class="txt--xl">📍</span> <?php echo $post_city_name; ?>, <?php echo $post_state_name; ?></p>
+    <p class="txt txt--xs"><span class="txt--xl">☑️</span> <?php echo $post_employment_type; ?></p>
+  </div>
+  <div class="wbjb-card__sub-hd">
+    <h3 class="txt txt--h4">Job Description</h3>
+  </div>
+  <div class="wpjb-card__bd">
+    <p class="txt txt--balance"><?php echo $decoded_job_description; ?></p>
+  </div>
+  <div class="wpjb-card__ft">
+    <div class="wpjb-utilityNav">
+      <button class="wpjb-utilityNav__btn txt--md">📩</button>
+      <button class="wpjb-utilityNav__btn txt--md" onclick="printContent()">⎙</button>
+      <button class="wpjb-utilityNav__btn txt--md">🔖</button>
     </div>
+  </div>
+</div>
