1c7f59b860f484df9700c8c1380e5dcac0b3c962
Justin Lawson <jstn.lawson@gmail.com>
2023-12-19 17:50:59 -0600

    styled modal added file upload js

--- public/js/wp-job-board-public.js
+++ public/js/wp-job-board-public.js
@@ -3,15 +3,15 @@
 
     // Form Modal
 
-  function showForm() {
-    var formOverlay = document.querySelector(".wpjb-form__modal-overlay");
-    formOverlay.style.display = "block";
-  }
+    function showForm() {
+        var formOverlay = document.querySelector(".wpjb-form__modal-overlay");
+        formOverlay.style.display = "flex";
+    }
 
-  function closeForm() {
-    var formOverlay = document.querySelector(".wpjb-form__modal-overlay");
-    formOverlay.style.display = "none";
-  }
+    function closeForm() {
+        var formOverlay = document.querySelector(".wpjb-form__modal-overlay");
+        formOverlay.style.display = "none";
+    }
 
     // dropdown menu function
 
@@ -41,6 +41,102 @@
             printWindow.close();
         };
     }
+
+    // Resume upload
+    $(document).on("DOMContentLoaded", function () {
+        const dragArea = document.querySelector(".wpjb-form__resume-drag");
+        const dragDropText = document.querySelector(".wpjb-form__resume-title");
+
+        let browse = document.querySelector(".wpjb-form__browse-link");
+        let browseInput = document.getElementById("wpjb-form__resume-browse");
+
+        browse.onclick = () => {
+            browseInput.click();
+        }
+
+        browseInput.addEventListener("change", function () {
+            console.log("file selected");
+            let fileType = browseInput.files[0].type;
+            console.log(fileType);
+
+            let validExtensions = [
+                "application/pdf",
+                "application/doc",
+                "application/docx",
+                "application/txt",
+                "application/rtf",
+                "application/odt",
+                "application/html",
+                "application/text",
+            ];
+
+            if (validExtensions.includes(fileType)) {
+                console.log("valid file type");
+                // Display "file uploaded" and the file name
+                dragArea.innerHTML = `${browseInput.files[0].name} upload successful!`;
+            } else {
+                console.log("invalid file type");
+                dragArea.innerHTML = `Invalid file type. <button class="wpjb-form__retry-btn" onclick="retryUpload()">Retry</button>`;
+            }
+        });
+
+        let file;
+
+        // when file is dragged over dragArea
+        dragArea.addEventListener("dragover", (event) => {
+            // console.log("file dragged over dragArea");
+            event.preventDefault();
+            dragDropText.textContent = "Release to Upload File";
+            dragArea.classList.add("active");
+        });
+
+        dragArea.addEventListener("dragleave", (event) => {
+            // console.log("file dragged left dragArea");
+            event.preventDefault();
+            dragDropText.textContent = "Drag & Drop";
+            dragArea.classList.remove("active");
+        });
+
+        dragArea.addEventListener("drop", (event) => {
+            // console.log("file dropped");
+            event.preventDefault();
+            file = event.dataTransfer.files[0];
+            // console.log(file);
+
+            let fileType = file.type;
+            console.log(fileType);
+
+            let validExtensions = [
+                "application/pdf",
+                "application/doc",
+                "application/docx",
+                "application/txt",
+                "application/rtf",
+                "application/odt",
+                "application/html",
+                "application/text",
+            ];
+
+            if (validExtensions.includes(fileType)) {
+                console.log("valid file type");
+                // Display "file uploaded" and the file name
+                dragArea.innerHTML = `File uploaded: ${file.name}`;
+            } else {
+                console.log("invalid file type");
+                dragArea.innerHTML = `Invalid file type. <button class="wpjb-form__retry-btn" onclick="retryUpload()">Retry</button>`;
+            }
+        });
+
+        function retryUpload() {
+            // Implement retry logic here
+            console.log("Retry button clicked");
+            dragArea.innerHTML = `Drag & Drop`;
+
+            // You can add code to reset the upload area or perform other actions.
+        }
+    });
+    // public functions
+
     window.closeForm = closeForm;
     window.showForm = showForm;
     window.printContent = printContent;
--- public/partials/wp-job-board-single.php
+++ public/partials/wp-job-board-single.php
@@ -82,7 +82,7 @@ if ($daysDifference > 0) {
       <h3>About the job</h3>
     </div>
     <div class="wpjb-card__bd">
-      <p class="txt txt--balance"><?php echo $post_job_description; ?></p>
+      <p class="txt txt-balance"><?php echo $post_job_description; ?></p>
     </div>
     <div class="wpjb-card__ft">
       <div class="wpjb-utilityNav">
@@ -112,29 +112,59 @@ if ($daysDifference > 0) {
   <div class="wpjb-form__modal-overlay">
     <div class="wpjb-form">
       <div class="wpjb-form__hd">
-        <h2>Apply for this job</h2>
+        <h1><?php the_title(); ?></h1>
+        <span class="wpjb-form__hd-subtitle txt-xs"><?php echo $post_city_name; ?>, <?php echo $post_state_name; ?> | <?php echo $post_employment_type; ?></span>
       </div>
       <div class="wpjb-form__bd">
         <form action="">
-          <div class="wpjb-form__group">
-            <label for="wpjb-form__group--name">Name</label>
-            <input type="text" id="wpjb-form__group--name" />
+          <div class="wpjb-form__firstName">
+            <label for="wpjb-form__firstName-input" aria-label="First Name"></label>
+            <input type="text" id="wpjb-form__firstName-input" class="wpjb-form__input" placeholder="First Name" />
           </div>
-          <div class="wpjb-form__group">
-            <label for="wpjb-form__group--email">Email</label>
-            <input type="email" id="wpjb-form__group--email" />
-          </div>
-          <div class="wpjb-form__group">
-            <label for="wpjb-form__group--phone">Phone</label>
-            <input type="tel" id="wpjb-form__group--phone" />
-          </div>
-          <div class="wpjb-form__group">
-            <label for="wpjb-form__group--resume">Resume</label>
-            <input type="file" id="wpjb-form__group--resume" />
-          </div>
-          <div>
-            <button onclick="closeForm()">Close</button>
-          </div>
+          <div class="wpjb-form__lastName">
+            <label for="wpjb-form__lastName-input" aria-label="Last Name"></label>
+            <input type="text" id="wpjb-form__lastName-input" class="wpjb-form__input" placeholder="Last Name" />
+            <div class="wpjb-form__email">
+              <label for="wpjb-form__email-input" aria-label="Email"></label>
+              <input type="email" id="wpjb-form__email-input" class="wpjb-form__input" placeholder="Email" />
+            </div>
+            <div class="wpjb-form__phone">
+              <label for="wpjb-form__phone-input" aria-label="Mobile Phone"></label>
+              <input type="tel" id="wpjb-form__phone-input" class="wpjb-form__input" placeholder="Mobile Phone" />
+            </div>
+
+            <div class="wpjb-form__resume">
+              <h4>Upload your resume</h4>
+              <div class="wpjb-form__resume-drag">
+                <span class="wpjb-form__resume-icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24" width="20" viewBox="0 0 384 512">
+                    <path opacity="1" fill="currentcolor" d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM216 408c0 13.3-10.7 24-24 24s-24-10.7-24-24V305.9l-31 31c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l72-72c9.4-9.4 24.6-9.4 33.9 0l72 72c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-31-31V408z" />
+                  </svg>
+                </span>
+                <span class="wpjb-form__resume-title">Drag & Drop</span>
+                <span class="wpjb-form__resume-description">
+                  or
+                  <label for="wpjb-form__resume-browse" aria-label="Resume Upload">
+                    <span class="wpjb-form__browse-link">
+                      browse
+                    </span>
+                    <input type="file" id="wpjb-form__resume-browse" style="display: none;" />
+                  </label>
+                </span>
+                <span class="wpjb-form__support">Supported file types: html,text,txt,pdf,doc,docx,rtf,odt </span>
+              </div>
+            </div>
+
+            <div class="wpjb-form__ft">
+              <button class="wpjb-form__close-btn" onclick="closeForm()">
+                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512">
+                  <path opacity="1" fill="#1E3050" d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" />
+                </svg>
+              </button>
+              <!-- <button class="wpjb-form__submit-btn">Submit</button>
+              <button onclick="closeForm()">Cancel</button> -->
+            </div>
+        </form>
       </div>
     </div>
   </div>
