93167e14679462720b5b102d45b08849282d96c0
Justin Lawson <jstn.lawson@gmail.com>
2024-01-06 10:19:56 -0600

    proper null checks on js page

--- public/js/wp-job-board-public.js
+++ public/js/wp-job-board-public.js
@@ -1,22 +1,34 @@
 (($) => {
     "use strict";
 
-    if (MicroModal !== null) {
-        MicroModal.init({
-            onShow: (modal) => console.info(`${modal.id} is shown`),
-            onClose: (modal) => console.info(`${modal.id} is hidden`),
-            openTrigger: "data-micromodal-trigger",
-            closeTrigger: "data-micromodal-close",
-            openClass: "wpjb-modal--isOpen",
-            disableScroll: true,
-            disableFocus: false,
-            awaitOpenAnimation: false,
-            awaitCloseAnimation: false,
-            debugMode: true,
-        });
+    /** ******************* */
+    /** MicroModal    */
+    /** ******************* */
+
+    const modalOpen = document.querySelector("data-micromodal-trigger");
+    const modalClose = document.querySelector("data-micromodal-close");
+
+    if (!modalOpen || !modalClose) {
+        return;
     }
 
-    // print function
+    MicroModal.init({
+        onShow: (modal) => console.info(`${modal.id} is shown`),
+        onClose: (modal) => console.info(`${modal.id} is hidden`),
+        openTrigger: "data-micromodal-trigger",
+        closeTrigger: "data-micromodal-close",
+        openClass: "wpjb-modal--isOpen",
+        disableScroll: true,
+        disableFocus: false,
+        awaitOpenAnimation: false,
+        awaitCloseAnimation: false,
+        debugMode: true,
+    });
+
+    /** ******************* */
+    /** Print Job Post      */
+    /** ******************* */
+
     function printContent() {
         console.log("print btn clicked!");
         const content = $("#wpjb-card").html();
@@ -35,7 +47,11 @@
     }
 
     $(document).on("DOMContentLoaded", function () {
-        // Resume upload
+
+        /** ******************* */
+        /** Upload Resume       */
+        /** ******************* */
+
         const dragArea = document.querySelector(".wpjb-drag__fieldset");
         const dragDropText = document.querySelector(".wpjb-drag__field-txt");
 
@@ -44,87 +60,93 @@
 
         const fileErrorSpan = document.querySelector(".wpjb-drag__file-error");
 
-        // confirm resume is attached on browse option
-        if (browseInput) {
-            browseInput.addEventListener("change", function () {
-                console.log("file selected");
-                if (this.files.length > 0) {
-                    dragArea.innerHTML = `✓ ${this.files[0].name} attached!`;
-                }
-            });
-        } else {
-            console.log("no browse input");
+        if (!browseInput || !dragDropText || !dragArea || !fileErrorSpan) {
+            return;
         }
+
+        // confirm resume is attached on browse option
+
+        browseInput.addEventListener("change", function () {
+            console.log("file selected");
+            if (this.files.length > 0) {
+                dragArea.innerHTML = `✓ ${this.files[0].name} attached!`;
+            }
+        });
 
         // Drag and drop resume functionality
 
-        if (dragArea && dragDropText && browseInput && fileErrorSpan) {
-            dragArea.addEventListener("dragover", (event) => {
-                // console.log("file dragged over dragArea");
-                event.preventDefault();
-                dragDropText.textContent = "Release to Upload File";
-                dragArea.classList.add("active");
-            });
+        dragArea.addEventListener("dragover", (event) => {
+            // console.log("file dragged over dragArea");
+            event.preventDefault();
+            dragDropText.textContent = "Release to Upload File";
+            dragArea.classList.add("active");
+        });
 
-            dragArea.addEventListener("dragleave", (event) => {
-                // console.log("file dragged left dragArea");
-                event.preventDefault();
-                dragDropText.textContent = "Drag & Drop";
-                dragArea.classList.remove("active");
-            });
+        dragArea.addEventListener("dragleave", (event) => {
+            // console.log("file dragged left dragArea");
+            event.preventDefault();
+            dragDropText.textContent = "Drag & Drop";
+            dragArea.classList.remove("active");
+        });
 
-            dragArea.addEventListener("drop", (event) => {
-                console.log("file dropped");
-                event.preventDefault();
-                file = event.dataTransfer.files[0];
-                console.log(file);
+        dragArea.addEventListener("drop", (event) => {
+            console.log("file dropped");
+            event.preventDefault();
+            file = event.dataTransfer.files[0];
+            console.log(file);
 
-                let fileType = file.type;
-                console.log(fileType);
+            let fileType = file.type;
+            console.log(fileType);
 
-                let validExtensions = [
-                    "application/pdf",
-                    "application/doc",
-                    "application/docx",
-                    "application/txt",
-                    "application/rtf",
-                    "application/odt",
-                    "application/html",
-                    "application/text",
-                ];
+            let validExtensions = [
+                "application/pdf",
+                "application/doc",
+                "application/docx",
+                "application/txt",
+                "application/rtf",
+                "application/odt",
+                "application/html",
+                "application/text",
+            ];
 
-                if (validExtensions.includes(fileType)) {
-                    console.log("valid file type");
-                    fileErrorSpan.style.opacity = "0";
-                    dragArea.innerHTML = `✓ ${file.name} attached!`;
-                } else {
-                    console.log("invalid file type");
-                    fileErrorSpan.style.opacity = "1";
-                }
-            });
-        }
+            if (validExtensions.includes(fileType)) {
+                console.log("valid file type");
+                fileErrorSpan.style.opacity = "0";
+                dragArea.innerHTML = `✓ ${file.name} attached!`;
+            } else {
+                console.log("invalid file type");
+                fileErrorSpan.style.opacity = "1";
+            }
+        });
 
-        //Search submit opacity
+        /** ******************* */
+        /** Search Functions    */
+        /** ******************* */
+
         const clearSearch = document.querySelector(".wpjb-btn__clearSettings");
         const searchSubmit = document.querySelector(".wpjb-search__submit");
 
-        if (searchSubmit !== null) {
+            if (!clearSearch || !searchSubmit) {
+                return;
+            }
+
             searchSubmit.addEventListener("click", function () {
                 console.log("search submit clicked");
                 clearSearch.style.opacity = "1";
             });
-        }
 
-        if (clearSearch !== null) {
             clearSearch.addEventListener("click", function () {
                 console.log("clear search clicked");
                 clearSearch.style.opacity = "0";
                 //add more clear functionality here
             });
-        }
+
     });
 
-    // Move input label up when input is filled
+    /** ******************* */
+    /** Form Labels         */
+    /** ******************* */
+
     function showLabel(labelId, input) {
         const label = document.getElementById(labelId);
         if (input.value.trim() !== "") {
@@ -135,6 +157,10 @@
             label.classList.add("hidden-label");
         }
     }
+
+    /** ******************* */
+    /** Show Filters    */
+    /** ******************* */
 
     function toggleFilters() {
         const facetSections = document.querySelectorAll(".wpjb-facet__section");
